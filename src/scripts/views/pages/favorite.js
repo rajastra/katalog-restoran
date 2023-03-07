9f6a927bd17f3d14cb90b4cd2416d330a1abf556
Raja Sa <rajasaputeraxii.ipa5@gmail.com>
2023-03-07 20:07:11 +0700

    update testing aplikasi has no id xit

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -15,6 +15,15 @@ const Favorite = {
   async afterRender() {
     const restaurants = await FavoriteRestoIdb.getAllResto();
     const restaurantContainer = document.querySelector(".explore__container");
+    if (restaurants.length === 0 || !restaurants) {
+      restaurantContainer.innerHTML = `
+      <div class="empty">
+        <h2 class="heading-secondary">No Restaurant Found</h2>
+      </div>
+      `;
+      document.querySelector(".explore h2").remove();
+      return;
+    }
     restaurants.forEach((restaurant) => {
       restaurantContainer.innerHTML += createRestaurantItemTemplate(restaurant);
     });
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -58,13 +58,13 @@ const createRestaurantItemTemplate = (restaurant) => `
 `;
 
 const createLikeButtonTemplate = () => `
-  <button aria-label="like this movie" id="likeButton" class="like">
+  <button aria-label="like this resto" id="likeButton" class="like">
     <i class="fa fa-heart-o" aria-hidden="true"></i>
   </button>
 `;
 
 const createLikedButtonTemplate = () => `
-  <button aria-label="unlike this movie" id="likeButton" class="like">
+  <button aria-label="unlike this resto" id="likeButton" class="like">
     <i class="fa fa-heart" aria-hidden="true"></i>
   </button>
 `;
